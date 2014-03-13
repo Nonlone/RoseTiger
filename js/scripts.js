5c270bcfb1459ac4a29c5f3309bcd0fc1df67505
Nonlone <thunderbird.shun@gmail.com>
2014-03-13 16:56:57 +0800

    修改tiger文件转成TimelineData文件，删除暂时没有用的js，修改脚本可以跑出时间轴

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,59 @@
 /**
   Index Script
 */
-$(document).ready(function(){
-	//render hook
+$(document).ready(function() {
+    //render hook
 
-	//scroll one page
-  $("#main").onepage_scroll({
-   	sectionContainer: "section", // 标记标签
-  	easing: "ease", // 动画模式 有"ease", "linear", "ease-in", "ease-out", "ease-in-out"
-  	animationTime: 1000, //动画时间
-   	pagination: true, // 展示导航
-   	updateURL: false, // 是否更新url  		
-   	loop: false, // 时候循环展示
-  	responsiveFallback: 600 // 响应式宽度布局，小于此值不表示
-  });
+    //scroll one page
+    $("#main").onepage_scroll({
+        sectionContainer: "section", // 标记标签
+        easing: "ease", // 动画模式 有"ease", "linear", "ease-in", "ease-out", "ease-in-out"
+        animationTime: 1000, //动画时间
+        pagination: true, // 展示导航
+        updateURL: false, // 是否更新url  		
+        loop: false, // 时候循环展示
+        responsiveFallback: 600 // 响应式宽度布局，小于此值不表示
+    });
 
-  //control-box
-  $(".controller").hover(function(){
+    //init timeline
+    createStoryJS({
+        type: 'timeline',
+        width: '800',
+        height: '600',
+        source: timelineData,
+        embed_id: 'timeline'
+    });
 
-    $(".controller").css("background","#808080");
+    //control-box
+    $(".controller").hover(function() {
 
-    $(this).css("background","#ffffff").prev("p").fadeIn(100);
+        $(".controller").css("background", "#808080");
 
-    if ($(this).is("#go_home")){
-        $("#text_home").show();
-    }
-    else if ($(this).is("#go_work")){
-        $("#text_work").show();
-    }
-    else if ($(this).is("#go_about")){
-        $("#text_about").show();
-    }
-    else if ($(this).is("#go_contact")){
-        $("#text_contact").show();
-    }
-    
-  });
+        $(this).css("background", "#ffffff").prev("p").fadeIn(100);
 
-  $(".controller").mouseleave(function(){
-      $(".btn-text").fadeOut(100);
-  });
+        if ($(this).is("#go_home")) {
+            $("#text_home").show();
+        } else if ($(this).is("#go_work")) {
+            $("#text_work").show();
+        } else if ($(this).is("#go_about")) {
+            $("#text_about").show();
+        } else if ($(this).is("#go_contact")) {
+            $("#text_contact").show();
+        }
+
+    });
+
+    $(".controller").mouseleave(function() {
+        $(".btn-text").fadeOut(100);
+    });
 
 
-  //发Email
-  $("#sendmail").click(function(){
-      var name = $("#name").val();
-      var email = $("#email").val();
-      var text = $("#text").val();
-      $(this).attr("href","mailto:team@rose-tiger.com?subject=Re:标题&body="+name + email +text);
-  });
+    //发Email
+    $("#sendmail").click(function() {
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var text = $("#text").val();
+        $(this).attr("href", "mailto:team@rose-tiger.com?subject=Re:标题&body=" + name + email + text);
+    });
 
 });
